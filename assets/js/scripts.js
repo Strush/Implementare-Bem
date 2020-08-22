48cee72ef5ee10141702539dcb8e3a0676c5abde
Petru <vinagapetru11@gmail.com>
2020-08-22 22:18:51 +0300

    New folder

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+
+// const navbar = document.querySelectorAll('.navbar__item__link ');
+ 
+// navbar.forEach(item => {
+//    item.addEventListener('click', (e) => {
+//        if(e.target )
+//     e.target.classList.add('navbar__item__link__active');
+//    });
+   
+// });
